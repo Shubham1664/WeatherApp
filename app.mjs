97ab48f750d3a2bb842bd3c674f033f9e65238ea
Shubham Verma <74960930+Shubham1664@users.noreply.github.com>
2023-05-31 18:52:50 +0530

    completed

--- app.mjs
+++ app.mjs
@@ -1,20 +1,41 @@
-import express from "express";
+import express, { response } from "express";
 import https from "https";
+import path from "path";
+import bodyParser from "body-parser";
 
 const app = express();
-const url = "https://api.openweathermap.org/data/2.5/weather?q=London&unit=metrics&appid=5344d8135f696a41e79f121e5649a7fc";
+app.use(bodyParser.urlencoded({extended:true}));
+const __mkdir =  path.resolve();
+
 
 app.get("/", (req,res)=>{
-
-    https.get(url, (response)=>{
-        
-        response.on("data", (data)=>{
-            const weatherData = JSON.parse(data);
-            console.log(weatherData);
-        })
+    res.sendFile(path.join(__mkdir, "index.html"));
     })
-    res.send("Server is up and running");
+    
+app.post("/", (req,res)=>{
+    
+    const location = req.body.cityName;
+    const apiKey = "5344d8135f696a41e79f121e5649a7fc";
+    const units = "metric";
+    const url = "https://api.openweathermap.org/data/2.5/weather?q="+ location +"&units=" + units +"&appid=" + apiKey;
+    https.get(url, (response)=>{
+        response.on("data", (data)=>{
+            let weatherData = JSON.parse(data);
+            const temp = weatherData.main.temp;
+            const weatherDescription = weatherData.weather[0].description;
+            const icon = weatherData.weather[0].icon;
+            const imageurl = "http://openweathermap.org/img/wn/"+ icon + "@2x.png";
+            res.write("<p>The weather is currently " + weatherDescription + "</p>");
+            res.write("<h1>Weather in " + req.body.cityName +" is " + temp + " degree Celsius</h1>");
+            res.write("<img src =" + imageurl + ">");
+            res.send();
+        })
+    });
+        
 })
+    
+
+    
 
 app.listen(3000, ()=>{
     console.log("I am listening");
